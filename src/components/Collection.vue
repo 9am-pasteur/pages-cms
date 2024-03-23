f1973d8a966a4fed79bf9eb6a3dcf0f2fd967025
Terunao HIROTA <thirota@ecw3.ioc.u-tokyo.ac.jp>
2024-03-23 23:55:46 +0900

    Adding link to public site in Collection view

--- src/components/Collection.vue
+++ src/components/Collection.vue
@@ -136,6 +136,12 @@
                       <template #content>
                         <ul>
                           <li>
+                            <a class="link w-full" :href="`${schema.siteprefix}${item.path.replace(/.*?\/([^\/]+)\.md$/,'$1')}${schema.sitepostfix}`" target="_blank">
+                              <div class="truncate">See file on IASA</div>
+                              <Icon name="ExternalLink" class="h-4 w-4 stroke-2 shrink-0 ml-auto text-neutral-400 dark:text-neutral-500"/>
+                            </a>
+                          </li>
+                          <li>
                             <a class="link w-full" :href="`https://github.com/${props.owner}/${props.repo}/blob/${props.branch}/${item.path}`" target="_blank">
                               <div class="truncate">See file on GitHub</div>
                               <Icon name="ExternalLink" class="h-4 w-4 stroke-2 shrink-0 ml-auto text-neutral-400 dark:text-neutral-500"/>
